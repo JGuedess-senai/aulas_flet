2483d92d2ea21fd020bc6f7efea66119acc1c22a
JGuedess-senai <joao.p.guedes8@aluno.senai.br>
2026-02-23 17:24:04 -0300

    primeiros passos em flet

---
+++ main.py
@@ -0,0 +1,14 @@
+import flet as ft
+# IMPORTA A BIBLIOTECA FLET E CRIA UM APELIDO(ALIAS)
+
+def main(page: ft.Page):
+    page.title = "Meu Primeiro App Flet" #Defie o título da janela
+    page.bgcolor = "blue"
+    page.vertical_alignment = ft.MainAxisAlignment.CENTER
+    page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
+    page.add(
+        ft.Text("Bem-Vindo ao meu App"),
+        ft.Text("Aqui você pode criar o que quiser!!")
+    )
+
+ft.run(main)
---
+++ onepiece.py
@@ -0,0 +1,19 @@
+import flet as ft
+
+def main(page: ft.Page):
+    def mostrar_mensagem(e):
+        page.add(
+            ft.Text("Eu vou ser o rei dos piratas.")
+        )
+    page.add(
+        ft.Text("Olá meu nome é Luffy!"),
+        ft.Image(
+            src="images/luffy.webp",
+            height=150
+        ),
+        ft.Button(
+            content="Clique Aqui",
+            on_click=mostrar_mensagem
+        )
+    )
+ft.run(main)
